439b11c452578c0aeddc0a63eb691c52b3c96c73
mjmarch33333 <marchmikej@gmail.com>
2016-05-07 07:40:00 -0400

    Adding email texting

--- app/Listeners/SendUserMessageNotification.php
+++ app/Listeners/SendUserMessageNotification.php
@@ -38,6 +38,7 @@ class SendUserMessageNotification {
         for ($y = 0; $y < count($managers); $y++)
         {
             $currentRow=$managers[$y];
+            // This is for email notifications
             if ($currentRow->email_notifications) 
             {
                 $data = array("deviceId"=>$event->deviceId, "email"=>$currentRow->email(), "name"=>$currentRow->last_name, "message"=>$event->message, "property"=>$device->address);
@@ -57,6 +58,31 @@ class SendUserMessageNotification {
                         $message->from(env('SEND_EMAIL', 'admin@tenantsyncdev.com'), 'TenantSync');
                     });
                 }
+            }
+
+            // This is for email text
+            if ($currentRow->text_notifications && $currentRow->cell_carrier > 0) 
+            {
+                $users = DB::table('cell_carriers')->where('id', '=', $currentRow->cell_carrier)->get();
+                $phone = $currentRow->phone . "@" . $users[0];
+                $data = array("deviceId"=>$event->deviceId, "email"=>$phone, "name"=>$currentRow->last_name, "message"=>$event->message, "property"=>$device->address);
+                if($currentRow->position == "Landlord") {                    
+                    Mail::raw($data['message'] . "\n" . env('URL_BASE', 'https://portal.tenantsync.com') . "/manager/device/" . $data['deviceId'],  function ($message) use ($data) {
+                        $message->to($data['email'],$data['name'])
+                            ->subject('Message received from ' . $data['property']);
+
+                        $message->from(env('SEND_EMAIL', 'admin@tenantsyncdev.com'), 'TenantSync');
+                    });
+                }
+                else 
+                {
+                    Mail::raw($data['message'] . "\n" . env('URL_BASE', 'https://portal.tenantsync.com') . "/landlord/device/" . $data['deviceId'],  function ($message) use ($data) {
+                        $message->to($data['email'],$data['name'])
+                            ->subject('Message received from ' . $data['property']);
+                        $message->from(env('SEND_EMAIL', 'admin@tenantsyncdev.com'), 'TenantSync');
+                    });
+                }
+            }
         }
 
 
