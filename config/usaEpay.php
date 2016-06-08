ea9d4836aab55d971d9cff4e41936fc58fa890f6
vagrant <mitchjam1928@gmail.com>
2016-06-08 23:15:23 +0000

    Added comment in usaEpay config file

--- config/usaEpay.php
+++ config/usaEpay.php
@@ -6,9 +6,11 @@ return [
 	
 	'soapUrl' => 'https://www.usaepay.com/soap/gate/0AE595C1/usaepay.wsdl',
 
+	// This is the key and pin for the TenantSync receiving account
 	'sourceKey' => '_OAo2776ws1599T3Y5yLvX65d4j5G0y0',
 	
 	'pin' => '1234',
+	//
 
 	'usaEpayPath' => base_path().'/app/Services/usaepay-php/usaepay.php',
 ];
